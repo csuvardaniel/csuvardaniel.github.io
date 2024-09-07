03f2c2bfb47be7833f78d2c52302bddc4d782454
csuvardaniel <csuvardaniel@proton.me>
2024-09-07 23:06:17 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,18 @@
 document.addEventListener('DOMContentLoaded', function() {
+    // Detect if the user is on mobile and load appropriate CSS
+    const userAgent = navigator.userAgent || navigator.vendor || window.opera;
+    const stylesheet = document.getElementById('stylesheet');
+    const deviceTypeElement = document.getElementById('deviceType'); // The device type element
+
+    if (/android|iPad|iPhone|iPod/.test(userAgent.toLowerCase())) {
+        stylesheet.setAttribute('href', 'mobile.css'); // Load mobile CSS
+        deviceTypeElement.textContent = "You are using a Mobile Device."; // Display mobile device info
+    } else {
+        stylesheet.setAttribute('href', 'pc.css'); // Load desktop CSS
+        deviceTypeElement.textContent = "You are using a Desktop Device."; // Display desktop device info
+    }
+
+    // Copy to clipboard functionality for email
     const copyBtns = document.querySelectorAll('.copy-btn');
     
     copyBtns.forEach(button => {
